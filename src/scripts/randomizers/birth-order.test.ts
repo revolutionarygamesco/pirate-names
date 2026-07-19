48499dc8b828091534cf55f696d94469c4208350
Jason Godesky <jefgodesky@gmail.com>
2026-07-19 13:12:29 -0400

    selectRandomBirthOrder: Don't return last anymore
    Our new FamilyContext class will take care of that. This method should just return the birth order as a number.

--- src/scripts/randomizers/birth-order.test.ts
+++ src/scripts/randomizers/birth-order.test.ts
@@ -1,14 +1,9 @@
 import { describe, it, expect } from 'vitest'
+import { isWithinRange } from '@revolutionarygamesco/common'
 import selectRandomBirthOrder from './birth-order.ts'
 
 describe('selectRandomBirthOrder', () => {
   it('returns a number between 1 and 13 or "last"', () => {
-    const actual = selectRandomBirthOrder()
-    if (typeof actual === 'string') {
-      expect(actual).toBe('last')
-    } else {
-      expect(actual).toBeGreaterThanOrEqual(1)
-      expect(actual).toBeLessThanOrEqual(13)
-    }
+    expect(isWithinRange(selectRandomBirthOrder(), [1, 13])).toBe(true)
   })
 })
--- src/scripts/randomizers/birth-order.ts
+++ src/scripts/randomizers/birth-order.ts
@@ -1,10 +1,9 @@
 import { selectRandomBetween } from '@revolutionarygamesco/common'
 import selectRandomFamilySize from './family.ts'
 
-const selectRandomBirthOrder = (): number | 'last' => {
+const selectRandomBirthOrder = (): number => {
   const familySize = selectRandomFamilySize()
-  const order = selectRandomBetween(1, familySize)
-  return familySize === order && familySize > 1 ? 'last' : order
+  return selectRandomBetween(1, familySize)
 }
 
 export default selectRandomBirthOrder
