b32c18082d149f510dc547ce841fa4e0fa32c051
Jason Godesky <jefgodesky@gmail.com>
2026-07-20 12:31:26 -0400

    Add  new class: BantuPersonalName

---
+++ src/scripts/classes/personal-names/bantu.test.ts
@@ -0,0 +1,114 @@
+import { beforeEach, describe, it, expect } from 'vitest'
+import { mockTables } from '@revolutionarygamesco/common-foundryvtt/mocks'
+import { genders, type Gender } from '../../enums/gender.ts'
+import BantuPersonalName, { BantuPersonalNameTables, type BantuPersonalNameData } from './bantu.ts'
+
+describe('BantuPersonalName', () => {
+  const data: BantuPersonalNameData = {
+    nationality: 'Bantu',
+    gender: 'Masculine',
+    full: 'Nzinga a Nkuwu',
+    personal: 'Nzinga',
+    father: 'Nkuwu',
+    special: null
+  }
+
+  beforeEach(() => {
+    mockTables({
+      [BantuPersonalNameTables.Init.Masculine]: { results: [{ description: 'Lema' } as foundry.documents.TableResult] },
+      [BantuPersonalNameTables.Init.Feminine]: { results: [{ description: 'Mabinda' } as foundry.documents.TableResult] },
+      [BantuPersonalNameTables.Nkumbu]: { results: [{ description: 'Kiala' } as foundry.documents.TableResult] },
+      [BantuPersonalNameTables.Santu]: { results: [{ description: 'Molazi' } as foundry.documents.TableResult] }
+    })
+  })
+
+  describe('constructor', () => {
+    it('creates a Bantu name', () => {
+      const actual = new BantuPersonalName()
+      expect(actual).toBeInstanceOf(BantuPersonalName)
+    })
+
+    it('sets nationality to Bantu', () => {
+      const actual = new BantuPersonalName()
+      expect(actual.nationality).toBe('Bantu')
+    })
+
+    it.each(genders)('can assign %s to gender', (gender: Gender) => {
+      const actual = new BantuPersonalName({ gender })
+      expect(actual.gender).toBe(gender)
+    })
+
+    it('randomizes the gender by default', () => {
+      const actual = new BantuPersonalName()
+      expect(genders).toContain(actual.gender)
+    })
+
+    it('can take a full name', () => {
+      const actual = new BantuPersonalName({ full: 'Nzinga a Nkuwu' })
+      expect(actual.full).toBe('Nzinga a Nkuwu')
+    })
+
+    it('defaults to Zola a Zola', () => {
+      const actual = new BantuPersonalName({ gender: 'Masculine' })
+      expect(actual.full).toBe('Zola a Zola')
+    })
+
+    it('can take a personal name', () => {
+      const actual = new BantuPersonalName({ personal: 'Nzinga' })
+      expect(actual.personal).toBe('Nzinga')
+    })
+
+    it('defaults to Zola for a personal name', () => {
+      const actual = new BantuPersonalName()
+      expect(actual.personal).toBe('Zola')
+    })
+
+    it('can take a father’s name', () => {
+      const actual = new BantuPersonalName({ father: 'Nkuwu' })
+      expect(actual.father).toBe('Nkuwu')
+    })
+  })
+
+  describe('Instance methods', () => {
+    describe('toObject', () => {
+      it('returns a data object', () => {
+        const instance = new BantuPersonalName(data)
+        const actual = instance.toObject()
+        expect(actual).toEqual(data)
+        expect(actual).not.toBe(data)
+      })
+    })
+  })
+
+  describe('Static methods', () => {
+    describe('generator', () => {
+      it('can generate a name', async () => {
+        const actual = await BantuPersonalName.generate({ special: null })
+        expect(actual.full).toBe('Kiala a Kiala')
+        expect(actual.personal).toBe('Kiala')
+        expect(actual.father).toBe('Kiala')
+      })
+
+      it('can generate a Christian name with a santu element', async () => {
+        const actual = await BantuPersonalName.generate({ special: 'Christian' })
+        expect(actual.full).toBe('Molazi Kiala a Kiala')
+        expect(actual.personal).toBe('Kiala')
+        expect(actual.father).toBe('Kiala')
+      })
+
+      it('can generate a name for an initiated man', async () => {
+        const actual = await BantuPersonalName.generate({ gender: 'Masculine', special: 'Initiated' })
+        expect(actual.full).toBe('Kiala a Kiala Lema')
+        expect(actual.personal).toBe('Kiala')
+        expect(actual.father).toBe('Kiala')
+      })
+
+      it('can generate a name for an initiated woman', async () => {
+        const actual = await BantuPersonalName.generate({ gender: 'Feminine', special: 'Initiated' })
+        expect(actual.full).toBe('Kiala a Kiala Mabinda')
+        expect(actual.personal).toBe('Kiala')
+        expect(actual.father).toBe('Kiala')
+      })
+    })
+  })
+})
---
+++ src/scripts/classes/personal-names/bantu.ts
@@ -0,0 +1,83 @@
+import { drawStr } from '@revolutionarygamesco/common-foundryvtt'
+import { selectRandomGender } from '../../enums/gender.ts'
+import PersonalName, { type PersonalNameData } from './base.ts'
+import getRollTableUUID from '../../get-rolltable-uuid.ts'
+import { selectRandomBetween } from '@revolutionarygamesco/common'
+
+type BantuSpecialNameType = 'Christian' | 'Initiated' | null
+
+export interface BantuPersonalNameData extends PersonalNameData {
+  father: string
+  special: BantuSpecialNameType
+}
+
+export const BantuPersonalNameTables = {
+  Init: {
+    Masculine: getRollTableUUID('P1mOexZPE3QmASEw'),
+    Feminine: getRollTableUUID('zk8wGuZMePWe4DaL')
+  },
+  Santu: getRollTableUUID('sHJcHKv4AL1xdrkT'),
+  Nkumbu: getRollTableUUID('FseoFZOSLwHCNy4W')
+}
+
+class BantuPersonalName extends PersonalName {
+  father: string
+  special: BantuSpecialNameType
+
+  constructor (data?: Partial<BantuPersonalNameData>) {
+    super(data)
+    this.nationality = 'Bantu'
+    this.personal = data?.personal ?? 'Zola'
+    this.father = data?.father ?? 'Zola'
+    this.special = ['Christian', 'Initiated'].includes(data?.special ?? '')
+      ? data!.special as BantuSpecialNameType
+      : null
+    this.full = data?.full ?? `${this.personal} a ${this.father}`
+  }
+
+  toObject (): BantuPersonalNameData {
+    return {
+      nationality: this.nationality,
+      gender: this.gender,
+      full: this.full,
+      personal: this.personal,
+      father: this.father,
+      special: this.special
+    }
+  }
+
+  static selectRandomSpecialNameType (): BantuSpecialNameType {
+    const r = selectRandomBetween(1, 20)
+    if (r < 11) return 'Christian'
+    if (r > 15) return 'Initiated'
+    return null
+  }
+
+  static async generate (
+    data?: Partial<BantuPersonalNameData>
+  ): Promise<BantuPersonalName> {
+    const generated: BantuPersonalNameData = {
+      nationality: 'Bantu',
+      gender: data?.gender ?? selectRandomGender(),
+      personal: data?.personal ?? await drawStr(BantuPersonalNameTables.Nkumbu, 'Zola'),
+      father: data?.father ?? await drawStr(BantuPersonalNameTables.Nkumbu, 'Zola'),
+      full: '',
+      special: data?.special === undefined ? null : data.special
+    }
+
+    if (generated.special === 'Christian') {
+      const santu = await drawStr(BantuPersonalNameTables.Santu, 'Ntoni')
+      generated.full = `${santu} ${generated.personal} a ${generated.father}`
+    } else if (generated.special === 'Initiated') {
+      const fallback = generated.gender === 'Masculine' ? 'Nsumbu' : 'Lubondo'
+      const initiated = await drawStr(BantuPersonalNameTables.Init[generated.gender], fallback)
+      generated.full = `${generated.personal} a ${generated.father} ${initiated}`
+    } else {
+      generated.full = `${generated.personal} a ${generated.father}`
+    }
+
+    return new BantuPersonalName(generated)
+  }
+}
+
+export default BantuPersonalName
