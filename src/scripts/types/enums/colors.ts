7a71d3ea65158970e2a6a0074ccf50430f99b78a
Jason Godesky <jefgodesky@gmail.com>
2026-07-24 16:45:55 -0400

    Colors: Removing ids.ts

--- src/scripts/types/enums/colors.ts
+++ src/scripts/types/enums/colors.ts
@@ -1,11 +1,12 @@
 import { makeEnum } from '@revolutionarygamesco/common'
 import { drawGuarded } from '@revolutionarygamesco/common-foundryvtt'
-import { nation } from '../../../ids.ts'
+import getRollTableUUID from '../../get-rolltable-uuid.ts'
 
 export const colors = ['Spanish', 'British', 'French', 'Dutch', 'Pirate'] as const
 export type Colors = typeof colors[number]
 export const { guard: isColors } = makeEnum(colors)
 
 export const selectRandomColors = async (): Promise<Colors> => {
-  return await drawGuarded(nation.ship, isColors, 'Spanish')
+  const uuid = getRollTableUUID('CrljZ2S8EdjWco9K')
+  return await drawGuarded(uuid, isColors, 'Spanish')
 }
